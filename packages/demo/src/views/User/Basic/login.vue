d78053e07b5f54ef3d851eda1a45257cb8ac8a29
zhouzhengyi <zhouzhengyi@xuetangx.com>
2023-07-30 15:10:29 +0800

    登录页面样式

--- packages/demo/src/views/User/Basic/login.vue
+++ packages/demo/src/views/User/Basic/login.vue
@@ -1,45 +1,46 @@
 <template>
-  <a-form
-    class="wrapper"
-    name="login-form"
-    autocomplete="off"
-    :model="formModel"
-    :label-col="{ span: 8 }"
-    :wrapper-col="{ span: 16 }"
-    @submit="onSubmit"
-  >
-    <a-form-item
-      label="用户名"
-      name="username"
-      :rules="[{ required: true, message: '请输入用户名！' }]"
+  <Card title="登录" style="width: 480px; margin: 20px auto">
+    <Form
+      name="login-form"
+      autocomplete="off"
+      :model="formModel"
+      :label-col="{ span: 4 }"
+      :wrapper-col="{ span: 20 }"
+      @submit="onSubmit"
     >
-      <a-input v-model:value="formModel.username" />
-    </a-form-item>
-    <a-form-item
-      label="密码"
-      name="password"
-      :rules="[{ required: true, message: '请输入密码' }]"
-    >
-      <a-input-password v-model:value="formModel.password" />
-    </a-form-item>
-    <a-form-item
-      label="验证码"
-      name="verificationCode"
-      :rules="[{ required: true, message: '验证码' }]"
-    >
-      <a-input v-model:value="formModel.verificationCode">
-        <template #addonAfter>
-          <img :src="verifacationUrl" @click="changeRandomNum" />
-        </template>
-      </a-input>
-    </a-form-item>
-    <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
-      <a-checkbox v-model:checked="formModel.remember">记住密码</a-checkbox>
-    </a-form-item>
-    <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
-      <a-button type="primary" html-type="submit">提交</a-button>
-    </a-form-item>
-  </a-form>
+      <FormItem
+        label="用户名"
+        name="username"
+        :rules="[{ required: true, message: '请输入用户名！' }]"
+      >
+        <a-input v-model:value="formModel.username" />
+      </FormItem>
+      <FormItem
+        label="密码"
+        name="password"
+        :rules="[{ required: true, message: '请输入密码' }]"
+      >
+        <a-input-password v-model:value="formModel.password" />
+      </FormItem>
+      <FormItem
+        label="验证码"
+        name="verificationCode"
+        :rules="[{ required: true, message: '验证码' }]"
+      >
+        <a-input v-model:value="formModel.verificationCode">
+          <template #addonAfter>
+            <img :src="verifacationUrl" @click="changeRandomNum" />
+          </template>
+        </a-input>
+      </FormItem>
+      <FormItem name="remember" :wrapper-col="{ offset: 8, span: 16 }">
+        <a-checkbox v-model:checked="formModel.remember">记住密码</a-checkbox>
+      </FormItem>
+      <FormItem :wrapper-col="{ offset: 8, span: 16 }">
+        <a-button type="primary" html-type="submit">提交</a-button>
+      </FormItem>
+    </Form>
+  </Card>
 </template>
 <script lang="ts">
 export default {
@@ -48,6 +49,8 @@ export default {
 </script>
 <script lang="ts" setup>
 import { ref, reactive, computed } from "vue";
+import { Card, Form, FormItem } from "ant-design-vue";
+
 const randomNum = ref("");
 const verifacationUrl = computed(() => {
   return `/api/user/verification/code?num_random=${randomNum.value}`;
