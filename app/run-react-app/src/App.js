71d7562cc3b24d562ca8bc79ff12e8bcdfd27a6e
zhouzhengyi <zhouzhengyi@xuetangx.com>
2024-12-12 00:09:54 +0800

    创建react 项目

---
+++ app/run-react-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { RefComp } from "./components/refComp/index";
+
+function App() {
+  return (
+    <div className="App">
+      <RefComp />
+    </div>
+  );
+}
+
+export default App;
