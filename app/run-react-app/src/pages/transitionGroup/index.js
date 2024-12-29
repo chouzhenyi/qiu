b540925c0bc9d3cf13b67b492a50d30b29f6ee6a
zhouzhengyi <zhouzhengyi@xuetangx.com>
2024-12-29 22:26:46 +0800

    transiton

--- app/run-react-app/src/pages/transitionGroup/index.js
+++ app/run-react-app/src/pages/transitionGroup/index.js
@@ -1,9 +1,35 @@
-// TODO css module css less
 import style from "./style.module.less";
+import { CSSTransition } from "react-transition-group";
+import React, { useState } from "react";
 
-const transitionGroup = () => {
-  console.log(style);
-  return <div>动画</div>;
+const TransitionGroup = () => {
+  const [show, setShow] = useState(true);
+  return (
+    <div className={style.wrapper}>
+      <div className={style.title}>动画</div>
+      <div>
+        <button type="button" onClick={() => setShow(!show)}>
+          动起来
+        </button>
+      </div>
+      <div className={style.transitionWrapper}>
+        <CSSTransition
+          in={show}
+          timeout={500}
+          classNames={{
+            enter: style.fadeEnter,
+            enterActive: style.fadeEnterActive,
+            enterDone: style.fadeEnterDone,
+            exit: style.fadeExit,
+            exitActive: style.fadeExitActive,
+            exitDone: style.fadeExitDone,
+          }}
+        >
+          <div className={style.box}></div>
+        </CSSTransition>
+      </div>
+    </div>
+  );
 };
 
-export default transitionGroup;
+export default TransitionGroup;
