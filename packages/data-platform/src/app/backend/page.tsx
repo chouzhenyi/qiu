e1bddbe3e2b59d7a0162431a5151a9b748f8c9a0
zhouzhengyi <zhouzhengyi@xuetangx.com>
2025-02-08 22:57:32 +0800

    拖拽

--- packages/data-platform/src/app/backend/page.tsx
+++ packages/data-platform/src/app/backend/page.tsx
@@ -1,18 +1,27 @@
+"use client";
 import "./style.css";
 import React from "react";
-
+import GridLayout from "react-grid-layout";
+import "react-grid-layout/css/styles.css"; // 引入默认样式
+// TODO: 拖拽元素进入编辑区域，编辑她的属性
 export default function Page() {
+  // layout is an array of objects, see the demo for more complete usage
+  const layout = [
+    { i: "a", x: 0, y: 0, w: 1, h: 2 },
+    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+    { i: "c", x: 4, y: 0, w: 1, h: 2 },
+  ];
   return (
-    <div className="wrapper">
-      <h1 className="title">台账搭建界面</h1>
-      <div className="designZone">
-        <div className="designLeft">
-          <div className="formZone"></div>
-          <div className="buttonZone"></div>
-          <div className="tableZone"></div>
-        </div>
-        <div className="designRight"></div>
-      </div>
-    </div>
+    <GridLayout
+      className="layout"
+      layout={layout}
+      cols={12}
+      rowHeight={30}
+      width={1200}
+    >
+      <div key="a">a</div>
+      <div key="b">b</div>
+      <div key="c">c</div>
+    </GridLayout>
   );
 }
